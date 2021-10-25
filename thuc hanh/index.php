32dc6864ef1ed557c4492fd0ecbbf0778103a9f5
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-10-25 16:40:58 +0700

    thuc hanh

---
+++ thuc hanh/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'DivideByZeroException.php';
+
+function divide($numerator, $denominator)
+{
+    if ($denominator === 0) {
+        throw new DivideByZeroException();
+    }
+    return $numerator / $denominator;
+}
+
+try {
+    $result = divide(100, 5);
+    echo $result;
+    $result = divide(100, 0);
+    echo $result;
+} catch (DivideByZeroException $e) {
+    echo ' Có lỗi xảy ra: ' . $e;
+}
